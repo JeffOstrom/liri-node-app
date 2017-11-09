53cec022c674e9f45ab74ef24813dea0566affc2
Jeff Ostrom <j.ostrm@gmail.com>
2017-11-09 09:51:05 -0600

    Initial updateJS layout for npm formats/functions/Switch layout

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 Synopsis
 -------------------------------------------------------------------------------------
 
-- LIRI is a Language Interpretation and Recognition Interfrace
+- LIRI is a Language Interpretation and Recognition Interfrace. LIRI is a command line node app that takes
+  parameters and returns it back data. 
 
 - Retrieving data from the following APIs 
 	- Twitter
 	- Spotify
-	- OMDB
+
+- Twitter: Display last 20 tweets 
+
+- Spotify: Display Artist, Song, Priview link, Album
+
 
 Built With
 -------------------------------------------------------------------------------------
---
+++ liri.js
@@ -0,0 +1,82 @@
+//make sure to save npm --save 
+
+var keysModule = require("./keys.js");
+
+var Twitter = require('twitter');
+var fs = require("fs");
+
+
+var command = process.argv[2]; 
+var argument = process.argv[3];
+
+runLiri();
+
+// var client = new Twitter(keysModule.twitterKeys);
+
+function runLiri(){
+
+	switch(command){
+		case "my-tweets":
+			twitterCode();
+			break;
+		case "spotify-this-song":
+			spotifyCode();
+			break;
+		case "movie-this":
+			movieCode()
+			break;
+		case "do-what-it-says":
+			doWhat();
+			break;
+		default:
+			console.log("please enter a Valid Command")
+	}
+}
+
+	function twitterCode(){
+		console.log("running twitter code");
+
+	}
+
+	function spotifyCode(){
+
+		if(argument === undefined)
+		{
+			argument = "The Sign";
+		}
+
+		console.log("running spot code");
+
+
+	}
+
+	function movieCode(){
+		if(argument === undefined)
+		{
+			argument = "Mr. Nobody";
+		}
+
+		console.log("running movie code", " movie:", argument);
+
+	}
+
+	function doWhat(){
+
+		fs.readFile("random.txt", "utf8", function(err, data){
+			console.log(data);
+
+			var data = "spotify-this-song,I Want it That Way";
+
+			var arr = data.split(",");
+
+
+			command = arr[0];
+			arrgument = arr[1];
+
+			runLiri();
+
+	});
+
+}
+
+

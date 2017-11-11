ecde86361eba8d415da1f8d74b99be207e78b2f6
Jeff Ostrom <j.ostrm@gmail.com>
2017-11-11 12:25:59 -0600

    Updated Twitter path npm/twitter functioning

--- keys.js
+++ keys.js
@@ -1,12 +1,12 @@
 console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: '<EdXyp94tKsXuoQmEQqYkDG9ph>',
-  consumer_secret: '<TmSIGDrx8GlgaBZ6lD2WYwUkj8YoMvuXzFfbFdoQzG5oLtdnHg>',
-  access_token_key: '<273468431-TVZetTmXiXrcCDUsf1ThDP8DDDiU4CsRr5JNdGpY>',
-  access_token_secret: '<cKxoeEQY34CeygMLK40JZuiXeZtK2M3ode912WuqaXsIN>',
+//Twittter npm key
+var twitterKeys = {
+  consumer_key: 'EdXyp94tKsXuoQmEQqYkDG9ph',
+  consumer_secret: 'TmSIGDrx8GlgaBZ6lD2WYwUkj8YoMvuXzFfbFdoQzG5oLtdnHg',
+  access_token_key: '273468431-TVZetTmXiXrcCDUsf1ThDP8DDDiU4CsRr5JNdGpY',
+  access_token_secret: 'cKxoeEQY34CeygMLK40JZuiXeZtK2M3ode912WuqaXsIN',
 }
 
-//module.exports = twitterKeys;
+ module.exports = twitterKeys;
 
 
--- liri.js
+++ liri.js
@@ -1,81 +1,97 @@
-//make sure to save npm --save 
+//npm key list
 var keysModule = require("./keys.js");
-// npm keys
 var Twitter = require('twitter');
 var fs = require("fs");
 
-// variable commands
+//Variable commands
 var command = process.argv[2]; 
 var argument = process.argv[3];
 
 runLiri();
 
 
-
+//If the function is called using switch method 
 function runLiri(){
-
 	switch(command){
 		case "my-tweets":
 			twitterCode();
 			break;
+
 		case "spotify-this-song":
 			spotifyCode();
 			break;
+
 		case "movie-this":
 			movieCode()
 			break;
+
 		case "do-what-it-says":
 			doWhat();
 			break;
+
 		default:
 			console.log("please enter a Valid Command")
-	}
-}
-
-	function twitterCode(){
-		console.log("running twitter code");
-
-	}
-
-	function spotifyCode(){
-
-		if(argument === undefined)
-		{
-			argument = "The Sign";
 		}
+	}//end of function
 
-		console.log("running spot code");
+//If the "twitterCode function is called"
+function twitterCode(){
 
+	console.log("this is working");
 
-	}
-
-	function movieCode(){
-		if(argument === undefined)
-		{
-			argument = "Mr. Nobody";
-		}
-
-		console.log("running movie code", " movie:", argument);
-
-	}
-
-	function doWhat(){
-
-		fs.readFile("random.txt", "utf8", function(err, data){
-			console.log(data);
-
-			var data = "spotify-this-song,I Want it That Way";
-
-			var arr = data.split(",");
-
-
-			command = arr[0];
-			arrgument = arr[1];
-
-			runLiri();
-
+	var client = new Twitter({
+	  consumer_key: keysModule.consumer_key,
+	  consumer_secret: keysModule.consumer_secret,
+	  access_token_key: keysModule.access_token_key,
+	  access_token_secret: keysModule.access_token_secret
 	});
 
-}
+	client.get("statuses/mentions_timeline",{count: 20}, function(err, tweets, response){
+		console.log(tweets);
+		//console.log(JSON.stringify(response));
+	})
+
+	}
+
+// 	function spotifyCode(){
+
+// 	// 	if(argument === undefined)
+// 	// 	{
+// 	// 		argument = "The Sign";
+// 	// 	}
+
+// 	// 	console.log("running spot code");
+
+
+// 	// }
+
+// 	// function movieCode(){
+// 	// 	if(argument === undefined)
+// 	// 	{
+// 	// 		argument = "Mr. Nobody";
+// 	// 	}
+
+// 	// 	console.log("running movie code", " movie:", argument);
+
+// 	// }
+
+// 	// function doWhat(){
+
+// 	// 	fs.readFile("random.txt", "utf8", function(err, data){
+// 	// 		console.log(data);
+
+// 	// 		var data = "spotify-this-song,I Want it That Way";
+
+// 	// 		var arr = data.split(",");
+
+
+// 	// 		command = arr[0];
+// 	// 		arrgument = arr[1];
+
+// 	// 		runLiri();
+
+// 	// });
+
+// }
 
 
